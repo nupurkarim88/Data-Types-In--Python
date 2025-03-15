ff50f1fba9f4fe700154fd9aa572369c24f10b70
nupurkarim88 <nupurkarim88@gmail.com>
2025-03-15 15:06:37 +0600

    Data Types in Python

---
+++ Type Casting.py
@@ -0,0 +1,28 @@
+#Assighning Different Variables
+
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("Name: ", name)
+print("Data Types of Age is: ", type(name))
+print("Age: ", age)
+print("Data Types of Age is: ", type(age))
+print("Is Student: ", is_student)
+print("Data Types of is_student is: ", type(is_student))
+print("Weight: ", weight)
+print("Data Types of Weight is: ", type(weight))
+
+# Type Casting
+
+print("\n After Type Casting...")
+age = str(age)
+print(age)
+print("Data Types of Age is: ", type(age))
+weight = int(weight)
+print (weight)
+print("Data Types of Weight is: ", type(weight))
+
+
+# Output:
