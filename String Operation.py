ebf1e22fae42701d6fd220b735598874b7fa62e3
nupurkarim88 <nupurkarim88@gmail.com>
2025-03-15 15:13:07 +0600

    Data Types In Python

---
+++ String Operation.py
@@ -0,0 +1,13 @@
+#input a word
+
+text = str(input("Enter a string: "))
+# Reverse String
+
+# using step value -1 to iteraqte in reverse order
+
+revText = text[::-1]
+text = revText
+print("Reverse of given String is:")
+print(text)
+
+#outputRahobar
